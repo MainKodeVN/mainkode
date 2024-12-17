54f65a0f21e89d58f4de46146533b2833f9d73ee
shrestic <nhatphongcgp@gmail.com>
2024-12-17 17:19:28 +0700

    create int layer manufacturing_cost

--- transform/mainkode_analytics/models/intermediate/manafacturing_cost/int_orders_summed_manufacturing_costs.sql
+++ transform/mainkode_analytics/models/intermediate/manafacturing_cost/int_orders_summed_manufacturing_costs.sql
@@ -3,7 +3,7 @@ WITH order_manufacturing_costs AS (
 )
 
 SELECT
-  MAX(order_id)                       AS order_id,
+  MAX(order_id)                 AS order_id,
   SUM(total_manufacturing_cost) AS total_manufacturing_costs
 FROM order_manufacturing_costs
 GROUP BY order_id
